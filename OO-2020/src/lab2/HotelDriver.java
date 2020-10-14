b3519219407b169fe7ebfc7daa490d521d84aad8
xenon <xenon@DESKTOP-RB0FU28>
2020-10-14 11:34:00 +0100

    Added a third room for Q3

--- OO-2020/src/lab2/HotelDriver.java
+++ OO-2020/src/lab2/HotelDriver.java
@@ -4,13 +4,24 @@ public class HotelDriver {
 	
 	public static void main(String[] args) {
 		
+		
+		
 		//New Objects
-		HotelRoom roomA = new HotelRoom(200 , "Single");
-		HotelRoom roomB = new HotelRoom(201, "Double");
+		HotelRoom roomA = new HotelRoom(200 , "Single" , 1 , 100);
+		HotelRoom roomB = new HotelRoom(201, "Double" , 0 , 80);
+		HotelRoom roomC = new HotelRoom(202, "Single", 0 , 90);
+		
 		
 		//PrintOuts
-		System.out.println("room number is " + roomA.getRoomNumber() + ",type is " + roomA.getRoomType());
-		System.out.println("room number is " + roomB.getRoomNumber() + ",type is " + roomB.getRoomType());
+		System.out.println("If Vancancy is 0 = Vacant , 1 = Occupied");
+		System.out.println("\nroom number is " + roomA.getRoomNumber() + " , type is " + roomA.getRoomType() + " , Vacancy= " 
+				+ roomA.getOccupancy() + " , Rate = " + roomA.getRate());
+		
+		System.out.println("\nroom number is " + roomB.getRoomNumber() + " , type is " + roomB.getRoomType() + " , Vacancy= " 
+				+ roomB.getOccupancy() + " , Rate = " + roomB.getRate());
+		
+		System.out.println("\nroom number is " + roomC.getRoomNumber() + " , type is " + roomC.getRoomType() + " , Vacancy= " 
+				+ roomC.getOccupancy() + " , Rate = " + roomC.getRate());
 		
 	}
 }
--- OO-2020/src/lab2/HotelRoom.java
+++ OO-2020/src/lab2/HotelRoom.java
@@ -1,9 +1,21 @@
 package lab2;
 
 public class HotelRoom {
+	
+	/*To enhance our room management system we want to know if the room is occupied
+	or vacant. Add a new private integer which can be either 0 or 1 (0 means vacant, 1
+	means occupied). In addition, we need to know what the nightly rate for each room
+	is. Add a double variable called rate. Write the necessary methods to set and get this
+	new variable. In the driver program set roomA to be occupied with a rate of 100, and
+	roomB to be unoccupied with a rate of 80 by calling the appropriate method.
+	Add these values to the output statements.*/
+	
+	
 	//Variable
 	private int roomNumber;
 	private String roomType;
+	private int occupancy;
+	private double rate;
 	
 	//Constructor
 	public HotelRoom() {
@@ -11,9 +23,11 @@ public class HotelRoom {
 		setRoomType("");
 	}
 	
-	public HotelRoom(int roomNumber, String roomType) {
+	public HotelRoom(int roomNumber, String roomType, int occupancy, double rate) {
 		setRoomNumber(roomNumber);
 		setRoomType(roomType);
+		setOccupancy(occupancy);
+		setRate(rate);
 	}
 	
 	
@@ -30,6 +44,25 @@ public class HotelRoom {
 	public void setRoomType(String roomType) {
 		this.roomType = roomType;
 	}
+
+	public int getOccupancy() {
+		return occupancy;
+	}
+
+	public void setOccupancy(int occupancy) {
+		this.occupancy = occupancy;
+	}
+
+	public double getRate() {
+		return rate;
+	}
+
+	public void setRate(double rate) {
+		this.rate = rate;
+	}
+	
+	//toString
+		
 	
 	
 }
