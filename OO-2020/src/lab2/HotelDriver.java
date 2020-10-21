fbb20d96f7c56c10380bdf089dbf70504cfdf4d2
xenon <xenon@DESKTOP-RB0FU28>
2020-10-21 17:01:09 +0100

    True and False Paramaters added , if
    driver program parameters are changed
    
    so will the result.

--- OO-2020/src/lab2/HotelDriver.java
+++ OO-2020/src/lab2/HotelDriver.java
@@ -1,27 +1,16 @@
 package lab2;
 
-public class HotelDriver {
-	
-	public static void main(String[] args) {
+public class HotelDriver 
+{	
+	public static void main(String[] args) 
+	{	
+		HotelRoom roomA = new HotelRoom(200, "Signle",true,100);
+		HotelRoom roomB = new HotelRoom(201, "Double",false,80);
 		
 		
+		System.out.println(roomA.toString());
+		System.out.println(roomB.toString());
 		
-		//New Objects
-		HotelRoom roomA = new HotelRoom(200 , "Single" , 1 , 100);
-		HotelRoom roomB = new HotelRoom(201, "Double" , 0 , 80);
-		HotelRoom roomC = new HotelRoom(202, "Single", 0 , 90);
-		
-		
-		//PrintOuts
-		System.out.println("If Vancancy is 0 = Vacant , 1 = Occupied");
-		System.out.println("\nroom number is " + roomA.getRoomNumber() + " , type is " + roomA.getRoomType() + " , Vacancy= " 
-				+ roomA.getOccupancy() + " , Rate = " + roomA.getRate());
-		
-		System.out.println("\nroom number is " + roomB.getRoomNumber() + " , type is " + roomB.getRoomType() + " , Vacancy= " 
-				+ roomB.getOccupancy() + " , Rate = " + roomB.getRate());
-		
-		System.out.println("\nroom number is " + roomC.getRoomNumber() + " , type is " + roomC.getRoomType() + " , Vacancy= " 
-				+ roomC.getOccupancy() + " , Rate = " + roomC.getRate());
 		
 	}
 }
--- OO-2020/src/lab2/HotelRoom.java
+++ OO-2020/src/lab2/HotelRoom.java
@@ -1,67 +1,88 @@
+
 package lab2;
 
 public class HotelRoom {
-	
-	/*To enhance our room management system we want to know if the room is occupied
-	or vacant. Add a new private integer which can be either 0 or 1 (0 means vacant, 1
-	means occupied). In addition, we need to know what the nightly rate for each room
-	is. Add a double variable called rate. Write the necessary methods to set and get this
-	new variable. In the driver program set roomA to be occupied with a rate of 100, and
-	roomB to be unoccupied with a rate of 80 by calling the appropriate method.
-	Add these values to the output statements.*/
-	
-	
-	//Variable
-	private int roomNumber;
 	private String roomType;
-	private int occupancy;
+	private int roomNumber;
 	private double rate;
+	private boolean occupancy;
 	
-	//Constructor
+	
+	/**
+	 * 
+	 */
+	
+	
+	
 	public HotelRoom() {
-		setRoomNumber(0);
-		setRoomType("");
+		
 	}
 	
-	public HotelRoom(int roomNumber, String roomType, int occupancy, double rate) {
+	public HotelRoom(int roomNumber, String roomTyp , boolean occupancy, double rate) {
+		
 		setRoomNumber(roomNumber);
+		
 		setRoomType(roomType);
+	
 		setOccupancy(occupancy);
+
 		setRate(rate);
 	}
 	
-	
-	//Getters and Setters
-	public int getRoomNumber() {
-		return roomNumber;
-	}
-	public void setRoomNumber(int roomNumber) {
-		this.roomNumber = roomNumber;
-	}
+	/*
+	 * 
+	 */
 	public String getRoomType() {
 		return roomType;
 	}
 	public void setRoomType(String roomType) {
 		this.roomType = roomType;
 	}
-
-	public int getOccupancy() {
-		return occupancy;
+	public int getRoomNumber() {
+		return roomNumber;
 	}
-
-	public void setOccupancy(int occupancy) {
-		this.occupancy = occupancy;
+	public void setRoomNumber(int roomNumber) {	
+		this.roomNumber = roomNumber;
 	}
-
+	 
 	public double getRate() {
 		return rate;
 	}
-
 	public void setRate(double rate) {
 		this.rate = rate;
 	}
+
+		
+	public boolean isOccupancy() {
+		OccupancyDecision();
+		return occupancy;
+	}
 	
-	//toString
+
+	public void setOccupancy(boolean occupancy) {
+		this.occupancy = occupancy;
+	}
+
+
+	@Override
+	public String toString() {
+			
+		return "Room number is " + getRoomNumber() + ", Type is " + getRoomType() + ". the room is " + OccupancyDecision() + " and the rate is " + getRate();
+			
+	}
+	
+	public String OccupancyDecision() {
+		if(this.occupancy == true) {
+			return "Occupied";
+		}
+		else {
+			return "Vacant";
+		}
+		
+	}
+		
+	
+	
 		
 	
 	
